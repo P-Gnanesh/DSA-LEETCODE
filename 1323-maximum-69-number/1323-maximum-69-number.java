70cb432437cc8794575bbd3b875df646d1acf80e
P-Gnanesh <gnaneshp2005@gmail.com>
2026-01-21 19:57:49 +0530

    Time: 0 ms (100%), Space: 42.2 MB (57.54%) - LeetHub

---
+++ 1323-maximum-69-number/1323-maximum-69-number.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int maximum69Number (int num) {
+        int count=0;
+        int temp=num;
+        while(temp>0)
+        {
+            count++;
+            temp/=10;
+        }
+
+        int arr[]=new int[count];
+        temp=num;
+        for (int i = count - 1; i >= 0; i--) {
+            arr[i] = temp % 10;
+            temp /= 10;
+        }
+
+
+        for (int i = 0; i < count; i++) {
+            if (arr[i] == 6) {
+                arr[i] = 9;
+                break;
+            }
+        }
+
+        int result = 0;
+        for (int i = 0; i < count; i++) {
+            result = result * 10 + arr[i];
+        }
+
+        return result;
+        
+    }
+}
